6d5d85125cc61801cea6a5e3717a708338921298
2nd <jazzwork03@gmail.com>
2024-11-08 01:38:45 +0530

    created register ui

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import "../css/Register.css";
 
 const Register = () => {
   return (
     <>
-        <div>
-            
+        <div className='div1'>
+          <form>
+            <h2>Register your form</h2>
+            <p>Enter your name:</p>
+            <input type="text" />
+            <p>Enter your email:</p>
+            <input type="email" />
+            <p>Enter your Phone No:</p>
+            <input type="number" />
+            <p>Enter your password:</p>
+            <input type="password" /><br />
+            <button>Submit</button>
+          </form>
         </div>
     </>
   )
